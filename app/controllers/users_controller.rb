545c069c915d0327eecc0260f988051ff5707fcf
Luigi del Rosario <luigidr97@gmail.com>
2018-12-06 22:23:11 +0800

    Now able to delete profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,9 +55,10 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+
     @user.destroy # destroy the user
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to logout_path, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
